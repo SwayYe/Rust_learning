900ca14564b576a83fbb4b71805ce64f24dc365a
swayye <swayye@sjtu.edu.cn>
2023-06-20 17:33:48 +0800

    updated to 4.1.3

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -24,8 +24,50 @@
 //     println!("{}, {}",s1,s2);  // no error
 // }
 
-fn main(){  // example of copy on stack
-    let x = 5;
-    let y = x;
-    println!("{}, {}", x, y);
+// fn main(){  // example of copy on stack
+//     let x = 5;
+//     let y = x;
+//     println!("{}, {}", x, y);
+// }
+
+// fn main(){
+//     let s = String::from("Hello World");
+//     take_ownership(s);  //此时s的值被移动到函数里面了，从这之后s就不再有效了
+//     let x =5;
+//     makes_copy(x);
+//     println!("x: {}", x);
+
+//     let s1 = gives_ownership(); //返回值所有权的移动
+//     let s2 = String::from("hello");
+//     let s3 = takes_and_gives_back(s2); //s2所有权移动到了s3
+// }
+
+fn take_ownership(some_string:String){
+    println!("{}", some_string)
+}   //函数结束时，会调用drop函数，some_string占用的内存被释放
+
+
+fn makes_copy(some_number:i32){
+    println!("{}", some_number);
+}   //这里的some_number是源数据的副本，函数结束时，只有副本被释放
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+
+fn main(){
+    let s1 = String::from("hello");
+    let (s2,len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+}    
+
+fn calculate_length(s:String) -> (String, usize){
+    let length = s.len();
+    (s,length)
 }
